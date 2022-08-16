aaabf653890b956ab6e9415e30c8db3eaeb5cb96
TZeigler <zeigt15@mail.sunysuffolk.edu>
2022-08-16 13:45:12 -0400

    A display of basic understanding

---
+++ Project_Clean.sql
@@ -0,0 +1,141 @@
+create database project_Clean
+go
+
+Alter Table Titles
+add constraint PK_Titles
+Primary Key (title_ID);
+
+Alter Table salaries
+add constraint PK_salaries
+Primary Key (emp_no);
+
+Alter table salaries
+drop constraint PK_salaries ;
+
+Alter Table Dept_manager
+add constraint PK_dept_manager
+Primary Key (emp_no);
+
+Alter Table title
+add constraint 
+
+Alter Table employees
+add constraint PK_employees
+Primary Key (emp_no);
+
+Alter Table employees
+Add Constraint DFT_employees_Hire_date
+Default (sysdatetime() ) for Hire_date;
+
+
+Alter Table departments
+add constraint PK_departments
+Primary Key (Dept_no);
+
+Alter Table departments
+add constraint fk_departments
+foreign key (dept_no)
+references dept_emp (dept_no);
+
+
+ALTER TABLE dept_emp with nocheck
+ADD CONSTRAINT FK_dept_emp
+Foreign key (Dept_no)
+References departments (dept_no);
+
+
+Alter Table Salaries
+add constraint fk_salaries
+foreign key (emp_no)
+references employees (emp_no);
+
+
+ALTER TABLE Titles
+ADD CONSTRAINT UNQ_Titles 
+UNIQUE (Title_ID);
+
+Alter Table dept_manager
+add constraint fk_dept_manager
+foreign key (emp_no)
+references employees (emp_no);
+
+
+
+Alter Table dept_emp
+add constraint PK_dept_emp
+Primary Key (emp_no);
+
+Create Index employees_info_idx
+on employees (emp_no,first_name,last_name);
+
+Alter Table dept_manager
+Add Constraint FK_dept_manager_departments
+foreign key (dept_no)
+references departments (dept_no)
+
+Alter Table employees add title_id nvarchar (50) null;
+
+Update employees
+Set title_id = emp_title_id;
+
+Alter table employees
+add constraint fk_employees_titles
+foreign key (title_id ) 
+references titles (title_id);
+
+
+---Part 2---
+
+Select employees.last_name, employees.first_name, employees.sex, salaries.salary
+from employees 
+join
+salaries on  employees.emp_no=salaries.emp_no
+
+Select first_name, last_name, hire_date
+from employees
+where hire_date  between '1986-01-01'
+and '1986-12-31';
+
+Select departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+from departments
+join dept_manager
+on departments.dept_no = dept_manager.dept_no
+join employees
+on employees.emp_no=dept_manager.emp_no
+order by dept_name asc;
+
+select employees.last_name, employees.first_name, departments.dept_name
+from employees
+join dept_emp
+on employees.emp_no=dept_emp.emp_no
+join departments
+on dept_emp.dept_no=departments.dept_no
+order by last_name asc;
+
+Select first_name, last_name, sex
+from employees
+where first_name = 'Hercules' and last_name like 'b%'
+order by last_name;
+
+select employees.last_name, employees.first_name, departments.dept_name
+from employees
+join dept_emp
+on employees.emp_no=dept_emp.emp_no
+join departments
+on dept_emp.dept_no=departments.dept_no
+where dept_name ='sales'
+order by last_name desc;
+
+select employees.last_name, employees.first_name, departments.dept_name
+from employees
+join dept_emp
+on employees.emp_no=dept_emp.emp_no
+join departments
+on dept_emp.dept_no=departments.dept_no
+where dept_name in('sales' ,'development')
+order by dept_name;
+
+Select last_name, COUNT (*) as total
+from employees
+group by last_name
+order by last_name desc;
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Project1
+
+This project is to be used as a snapshot of my abilities and understanding of SQL
+The first part of the display is “Data Engineering” or “Data Modeling.” It is important to know the tables within the database and the information of which the tables consist of. 
+Once I identified the tables and its information, I was able to create the schema or relational diagram to link the tables.
+There is one table within the diagram that did not have a column with a common name to any other table. Upon review there were two tables with the same values. I duplicated the column with the matching values and gave it the same name to create the relationship in the schema.
+The second part of this display is Data Analytics. 
+1. To identify each employee’s employee number, last name, first name, sex, and salary on one table. I used the “select” function, listed all values desired and joined tables for all information to be present in one search.
+2. To identify the employees hired in 1986, I selected the columns to be identified. I used the “where” function for the hire_date column. I then used the “between” function to pull just the employees hired in 1986.
+3. The information that was requested was not available on one table. In this matter I used the “join” function twice to join three tables. I used “order by” the department name for structure. In this request the department name seems to be the highlight of the request.
+4. The information that was requested was not available on one table. In this matter I used the “join” function twice to join three tables. I used “order by” the employee’s last name for structure. In this request the employee information seems to be the highlight of the request.
+5. I used the “where” function to pull the first name “Hercules”. To add to the desired results for those with the last names starting with “B” on the same table, I used the “and” and “like” functions. Since the first names are the same, I used “order by” last name.
+6. For this request I had to “join” multiple tables. The desired information was of those employees in the sale department. I used the “where” function to identify sales in the dept_name column using “=”.
+7. For this request I had to “join” multiple tables. The desired information was of those employees in the sale and development departments. I used the “where” function to identify “sales” and “development” in the dept_name column using “in.”
+8. For this task I used the “count” function to get the total number of each last name. I named the column for the count (total). I “group by” and “order by” the last name and put it in “desc” order.
